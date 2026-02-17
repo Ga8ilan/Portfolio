00a6983df3815c50fbfce8551f7c180a936501d8
Edgar Castaneda <castaneda.e@northeastern.edu>
2026-02-17 16:20:21 -0500

    working on about

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ function NavBar() {
     return (
         <nav className="navbar">
             <div className="nav-container">
-                <div className="logo">
-                    <img src={E} axlt="Edgar Logo" className="logo">
-                    </img>
-                </div>
                 <ul className="nan-links">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#Contact">Contact</a></li>
+                    <li>
+                        <a href="about">About
+                            <img src={E} alt="Edgar Logo" className="logo" />
+                        </a>
+                    </li>
+                    <li><a href="projects">Projects</a></li>
+                    <li><a href="contact">Contact</a></li>
                 </ul>
             </div>
         </nav>
---
+++ src/components/about.jsx
@@ -0,0 +1,22 @@
+// About section of my website
+
+<section id="about" className="about">
+    <h2>Edgar Castaneda</h2>
+    <div className="about-content">
+        <p1>
+            Hi! I'm Edgar Castaneda. I study computer science at //Northeastern Univeristy// and
+            I'm concentrating in Software! I'm passionate about the things I build and I truly
+            enjoy spending time working on my work. I'm always excited to learn new things and
+            //bring my very best to whatever I need to do//. 
+
+            I love connecting and talking to new people so please don't hesitate to contact me.
+            I'm open to building new things and helping out on projects, and I'm always down for
+            just a simple chat.
+
+            I've been programming for the past two years and I've learned so much. I've recently
+            become a fellow ColorStack and I'm excited to join and experience even more new things.
+        </p1>
+    </div>
+</section>
+
+export default about
