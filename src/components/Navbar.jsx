46db1cd20adbb581d998fe2e0c93f84ae036351c
Edgar Castaneda <castaneda.e@northeastern.edu>
2026-02-17 15:59:35 -0500

    navbar start'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
+import E from '../assets/E.png'
+
 function NavBar() {
     return (
-        <nav>
-            <h2>Portfolio</h2>
+        <nav className="navbar">
+            <div className="nav-container">
+                <div className="logo">
+                    <img src={E} axlt="Edgar Logo" className="logo">
+                    </img>
+                </div>
+                <ul className="nan-links">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#Contact">Contact</a></li>
+                </ul>
+            </div>
         </nav>
     );
 }
